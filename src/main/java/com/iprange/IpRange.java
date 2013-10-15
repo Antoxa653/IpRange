ab393828ac9d8b5d51d8ff0e1f379111f8899431
Anton Rykov <anton.rykov@gmail.com>
2013-10-15 19:00:24 +0300

    second commit

--- src/main/java/com/iprange/IpRange.java
+++ src/main/java/com/iprange/IpRange.java
@@ -1,13 +1,13 @@
 package com.iprange;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
 
 public class IpRange {
 
 	public List<String> proccessIpRange(String ip1, String ip2) {
-		List<String> resultList = new LinkedList();
+		List<String> resultList = new ArrayList();
 		if (ip1 != null && ip2 != null) {
 			String regex = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\.([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
 			if (ip1.matches(regex) && ip2.matches(regex)) {
@@ -18,29 +18,17 @@ public class IpRange {
 				for (int i = 0; i < array1String.length; i++) {
 					array1[i] = Integer.parseInt(array1String[i]);
 					array2[i] = Integer.parseInt(array2String[i]);
-				}
-				array1[array1.length - 1]++;
-				boolean check = true;
+				}				
+				boolean check = false;
 				for (int i = 0; i < array1.length; i++) {
-					if (array1[i] <= array2[i]) {
+					if (array1[i] < array2[i]) {
 						check = true;
-					}
-					else {
-						check = false;
+						break;
 					}
 				}
 				if (check) {
+					StringBuilder sb = new StringBuilder();
 					while (!Arrays.equals(array1, array2)) {
-						System.out.println(Arrays.toString(array1));
-						StringBuilder sb = new StringBuilder();
-						for (int i = 0; i < array1.length; i++) {							
-							sb.append(array1[i]);
-							if (i < 3) {
-								sb.append(".");
-							}
-						}
-						resultList.add(sb.toString());
-
 						array1[array1.length - 1]++;
 						for (int i = array1.length - 1; i > 0; i--) {
 							if (array1[i] > 255) {
@@ -51,6 +39,17 @@ public class IpRange {
 								break;
 							}
 						}
+						if (!Arrays.equals(array1, array2)) {
+							System.out.println(Arrays.toString(array1));
+							for (int i = 0; i < array1.length; i++) {
+								sb.append(array1[i]);
+								if (i < 3) {
+									sb.append(".");
+								}
+							}
+							resultList.add(sb.toString());
+							sb.setLength(0);
+						}
 					}
 					System.out.println("Proccessing done!");
 				}
@@ -58,7 +57,6 @@ public class IpRange {
 					throw new IllegalArgumentException("First ip adress must be lower than second");
 				}
 			}
-
 			else {
 				throw new IllegalArgumentException("Bad ipv4 adress format it must be string like 255.255.255.255");
 			}
@@ -67,13 +65,8 @@ public class IpRange {
 			throw new NullPointerException("Ip adresses must be not null");
 		}
 		return resultList;
-
 	}
 
-	public static void main(String[] args) {
-		IpRange ir = new IpRange();
-		String testArray1 = "0.0.0.0";
-		String testArray2 = "0.0.0.10";
-		ir.proccessIpRange(testArray1, testArray2);
+	public static void main(String[] args) {	
 	}
 }
--- src/test/java/com/iprange/tests/IpRangeTest.java
+++ src/test/java/com/iprange/tests/IpRangeTest.java
@@ -10,56 +10,63 @@ import org.junit.Test;
 import com.iprange.IpRange;
 
 public class IpRangeTest {
-	
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testOverflowParam() {
 		String testArray1 = "-1.0.0.0";
 		String testArray2 = "-2.0.0.0";
-		IpRange ipRange = new IpRange();		
+		IpRange ipRange = new IpRange();
 		ipRange.proccessIpRange(testArray1, testArray2);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testFirstIpMoreThenSecondIp() {
 		String testArray1 = "0.0.0.1";
 		String testArray2 = "0.0.0.0";
-		IpRange ipRange = new IpRange();		
+		IpRange ipRange = new IpRange();
 		ipRange.proccessIpRange(testArray1, testArray2);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testFirstIpEqualsSecondIp() {
 		String testArray1 = "0.0.0.0";
 		String testArray2 = "0.0.0.0";
-		IpRange ipRange = new IpRange();		
+		IpRange ipRange = new IpRange();
 		ipRange.proccessIpRange(testArray1, testArray2);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testFirstIpBadFormat() {
 		String testArray1 = "0.0.0";
 		String testArray2 = "0.0.0.0";
-		IpRange ipRange = new IpRange();		
+		IpRange ipRange = new IpRange();
 		ipRange.proccessIpRange(testArray1, testArray2);
 	}
-	
+
 	@Test(expected = NullPointerException.class)
 	public void testNullIpArrays() {
-		String testArray1 = null; 
+		String testArray1 = null;
 		String testArray2 = null;
-		IpRange ipRange = new IpRange();		
+		IpRange ipRange = new IpRange();
 		ipRange.proccessIpRange(testArray1, testArray2);
 	}
-	
+
 	@Test(expected = IllegalArgumentException.class)
 	public void testInvalidSeparator() {
 		String testArray1 = "0-0-0-1";
 		String testArray2 = "0-0-0-0";
-		IpRange ipRange = new IpRange();		
+		IpRange ipRange = new IpRange();
 		ipRange.proccessIpRange(testArray1, testArray2);
 	}
-	
+
+	@Test(expected = IllegalArgumentException.class)
+	public void testFirstIpCharacters() {
+		String testArray1 = "a.b.c.d";
+		String testArray2 = "0.0.0.0";
+		IpRange ipRange = new IpRange();
+		ipRange.proccessIpRange(testArray1, testArray2);
+	}
+
 	@Test
 	public void testValidIps() {
 		String testArray1 = "0.0.0.0";
